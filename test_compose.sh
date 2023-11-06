473b2061252b88c677a041e6a2028df78b70a060
lzq <lzq47@foxmail.com>
2023-11-06 16:15:41 +0800

    add all output  in test_compose

--- test_compose.sh
+++ test_compose.sh
@@ -1,26 +1,47 @@
-echo $1
-rm -rf  $1/target/
-mkdir -p $1/target/
-for config in $(ls $1/*.yaml);do
+# DIR is the folder path of test compose
+DIR=$1
+echo $DIR
+rm -rf  $DIR/target/
+mkdir -p $DIR/target/
+
+# Clusters is the clusters that will be tested
+Clusters=""
+ls $DIR/*.run  >/dev/null 2>&1
+if [ $? -eq 0 ];then
+    for clusterf in $(ls $DIR/*.run);do
+        temp=$(basename $clusterf)
+        # append cluster 
+        Clusters="$Clusters ${temp/%".run"}"
+    done
+else
+    # test all cluster by default
+    Clusters="center share dcss"
+fi
+
+basename_no_extension(){
+    base=$(basename $1)
+    echo ${base/%"$2"}
+}
+
+
+for config in $(ls $DIR/*.yaml);do
     echo $config
-    base=$(basename $config)
-    configname=${base/%".yaml"}
-    cluster=""
-    ls $1/*.run 2>/dev/null
-    if [ $? -eq 0 ];then
-        for clusterf in $(ls $1/*.run);do
-            temp=$(basename $clusterf)
-            cluster="$cluster ${temp/%".run"}"
-        done
-    else
-        cluster="center share dcss"
-    fi
-
-    echo "start test from $config for $cluster"
-    for c in $cluster;do
-
+    configname=$(basename_no_extension $config '.yaml')
+    echo "start test from $config for $Clusters"
+    for c in $Clusters;do
         echo "start test from $config for $c"
         make ${c}Test Config=$config TargetFolder="$1/target/${configname}_${c}" > /tmp/test_compose.log 2>&1
         if [ $? -eq 0 ];then echo "successd";else cat /tmp/test_compos.log;exit  1 ;fi
     done
 done
+
+mkdir -p $DIR/target/all
+cp $DIR/*.yaml $DIR/target/all
+for config in $(ls $DIR/*.yaml);do
+    echo $config
+    configname=$(basename_no_extension $config '.yaml')
+    for c in $Clusters;do
+        cp $DIR/target/${configname}_${c}/cluster_status.png $DIR/target/all/${configname}_${c}.png
+    done
+done
+
