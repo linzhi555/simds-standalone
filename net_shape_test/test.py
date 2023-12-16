99ef963588ac776495b1913ebd8e5fefb722ed48
lzq <lzq47@foxmail.com>
2023-12-16 22:57:05 +0800

    work on net_shape_test

--- net_shape_test/test.py
+++ net_shape_test/test.py
@@ -22,8 +22,8 @@ class Cluster():
 clusters = []
 clusters.append(Cluster("center","centralized cluster","make centerTest"))
 clusters.append(Cluster("share","shared cluster","make shareTest"))
-clusters.append(Cluster("dcss_regular","dcss_regular","make dcssTest", specialConfig={'DcssNeiborRandomP':0.0}))
-clusters.append(Cluster("dcss_random","dcss_random","make dcssTest",specialConfig={'DcssNeiborRandomP':0.5}))
+clusters.append(Cluster("dcss_regular","Hive regular neighbor","make dcssTest", specialConfig={'DcssNeiborRandomP':0.0}))
+clusters.append(Cluster("dcss_random","Hive random neighbor","make dcssTest",specialConfig={'DcssNeiborRandomP':0.5}))
 print(clusters)
 
 nodeNumTests = [1,2,4,7,10]
@@ -33,7 +33,7 @@ def test_node_num():
             configCopy = config.copy()
             configCopy["NodeNum"] = nodeNum * 1000
             configOut = "net_shape_test/config.yaml"
-            targetOut = "net_shape_test/target/{}_{}k".format(cluster.name,nodeNum)
+            targetOut = "net_shape_test/target/node_num/{}_{}k".format(cluster.name,nodeNum)
             if cluster.specialConfig != None:
                 for k,v in cluster.specialConfig.items():
                     configCopy[k]=v
@@ -41,11 +41,11 @@ def test_node_num():
                 yaml.dump(configCopy,output)
             os.system("{} Config={} TargetFolder=\"{}\"".format(cluster.command,configOut,targetOut))
 
-def draw_node_num():
+def draw_node_num_test():
     for nodeNum in nodeNumTests:
         tests = []
         for cluster in clusters:
-            folder = "net_shape_test/target/{}_{}k".format(cluster.name,nodeNum)
+            folder = "net_shape_test/target/node_num/{}_{}k".format(cluster.name,nodeNum)
             tests.append([folder, "{c} {num}k nodes".format(c=cluster.describ,num=nodeNum)])
         draw.draw_task_submission_rate(tests)
         draw.draw_muilt_lantencyCurve(tests)
@@ -53,9 +53,105 @@ def draw_node_num():
         draw.draw_muilt_var_resource (tests)
         draw.draw_muilt_net_busy (tests)
         draw.draw_task_latency_CDF(tests)
-        os.system("mkdir -p net_shape_test/target/all/nodes_{num}k && mv *.png net_shape_test/target/all/nodes_{num}k".format(num=nodeNum))
+        os.system("mkdir -p net_shape_test/target/node_num/all/nodes_{num}k && mv *.png net_shape_test/target/node_num/all/nodes_{num}k".format(num=nodeNum))
+
+possblities = [0.0,0.3,0.6,1.0]
+
+def test_neibor_random_P():
+    for p in possblities:
+        cluster = clusters[3] # dcss_random
+        configCopy = config.copy()
+        configCopy["NodeNum"] =  4 * 1000
+        configCopy["DcssNeiborRandomP"] = p
+        configOut = "net_shape_test/config.yaml"
+        targetOut = "net_shape_test/target/neibor_random_p/randomP_{}".format(p)
+        with open(configOut, "w") as output:
+            yaml.dump(configCopy,output)
+        os.system("{} Config={} TargetFolder=\"{}\"".format(cluster.command,configOut,targetOut))
+
+def draw_neibor_random_P_test():
+    tests = []
+    for p in possblities:
+        cluster = clusters[3]
+        folder = "net_shape_test/target/neibor_random_p/randomP_{}".format(p)
+        tests.append([folder, "random neighbor p {} ".format(p)])
+    draw.draw_task_submission_rate(tests)
+    draw.draw_muilt_lantencyCurve(tests)
+    draw.draw_muilt_avg_resource (tests)
+    draw.draw_muilt_var_resource (tests)
+    draw.draw_muilt_net_busy (tests)
+    draw.draw_task_latency_CDF(tests)
+    os.system("mkdir -p net_shape_test/target/all/neibor_random_p && mv *.png net_shape_test/target/all/neibor_random_p")
+
+TaskNumFactors = [6.0,6.2,6.4,6.6,6.8]
+utilizations = ["80%","83%","85%","88%","90%"]
+
+def test_utilization():
+    for factor,util in zip(TaskNumFactors,utilizations):
+        for cluster in clusters:
+            configCopy = config.copy()
+            configCopy["TaskNumFactor"] = factor
+            configOut = "net_shape_test/config.yaml"
+            targetOut = "net_shape_test/target/utilization/{}_{}".format(cluster.name,util)
+            if cluster.specialConfig != None:
+                for k,v in cluster.specialConfig.items():
+                    configCopy[k]=v
+            with open(configOut, "w") as output:
+                yaml.dump(configCopy,output)
+            os.system("{} Config={} TargetFolder=\"{}\"".format(cluster.command,configOut,targetOut))
+
+def draw_uliliztion_test():
+    for cluster in clusters:
+        tests = []
+        for factor,ulti in zip(TaskNumFactors,utilizations):
+            folder = "net_shape_test/target/utilization/{}_{}".format(cluster.name,ulti)
+            tests.append([folder, "{c}  ".format(c=cluster.describ)])
+        draw.draw_task_submission_rate(tests)
+        draw.draw_muilt_lantencyCurve(tests)
+        draw.draw_muilt_avg_resource (tests)
+        draw.draw_muilt_var_resource (tests)
+        draw.draw_muilt_net_busy (tests)
+        draw.draw_task_latency_CDF(tests)
+        os.system("mkdir -p net_shape_test/target/all/utilization/{} && mv *.png net_shape_test/target/all/utilization/{}".format(cluster.name,cluster.name))
+
+#def test_with_parameters(paramsName,paramsList):
+#    for param in paramsList:
+#        for cluster in clusters:
+#            configCopy = config.copy()
+#            configCopy[paramsName] = param
+#            configOut = "net_shape_test/config.yaml"
+#            targetOut = "net_shape_test/target/{}/{}_{}".format(paramsName,cluster.name,param)
+#            if cluster.specialConfig != None:
+#                for k,v in cluster.specialConfig.items():
+#                    configCopy[k]=v
+#            with open(configOut, "w") as output:
+#                yaml.dump(configCopy,output)
+#            os.system("{} Config={} TargetFolder=\"{}\"".format(cluster.command,configOut,targetOut))
+#
+#def draw_test_result(paramsName,paramsList):
+#    for param in paramsList:
+#        tests = []
+#        for cluster in clusters:
+#            folder = "net_shape_test/target/{}/{}_{}".format(paramsName,cluster.name,param)
+#            tests.append([folder, "{} {}".format(cluster.name,param)])
+#        draw.draw_task_submission_rate(tests)
+#        draw.draw_muilt_lantencyCurve(tests)
+#        draw.draw_muilt_avg_resource (tests)
+#        draw.draw_muilt_var_resource (tests)
+#        draw.draw_muilt_net_busy (tests)
+#        draw.draw_task_latency_CDF(tests)
+#        os.system("mkdir -p net_shape_test/target/{}/all/ && mv *.png net_shape_test/target/{}/all/".format(paramsName,paramsName))
+
+    #test_with_parameters("TaskNumFactor",utilizationTests)
+    #draw_test_result("TaskNumFactor",utilizationTests)
 
 
-test_node_num()
-draw_node_num()
+if __name__ == "__main__":
+    test_node_num()
+    draw_node_num_test()
 
+    test_neibor_random_P()
+    draw_neibor_random_P_test()
+
+    test_utilization()
+    draw_uliliztion_test()
