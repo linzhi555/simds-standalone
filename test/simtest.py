46abe3f49ca7a25c9c37557b1edfccd21c01b131
lzq <lzq47@foxmail.com>
2024-09-22 18:47:02 +0800

    add calibre func in simtest

--- test/simtest.py
+++ test/simtest.py
@@ -19,11 +19,10 @@ def load_config(filepath: str = "config_template.yaml") -> dict:
 
 class Cluster:
 
-    def __init__(self, name, describ, command, specialConfig=None):
+    def __init__(self, name, describ, command,):
         self.name = name
         self.describ = describ
         self.command = command
-        self.specialConfig = specialConfig
 
 
 centerCluster = Cluster("center", "central", "make test Cluster=Center")
@@ -43,14 +42,74 @@ shareK8sCluster = Cluster("sharek8s", "share deploy",
 dcssK8sCluster = Cluster("dcssk8s", "dcss deploy ",
                          "make k8sTest Cluster=Dcss")
 
+# 比较不同参数的软件模拟的集群和固定参数的k8s集群部署的表现"""
+# 为了校准得到，哪个参数最贴近实际情况 """
+
+
+def calibre(
+        config: map,
+        simCluster: Cluster,
+        k8sCluster: Cluster,
+        testname: str,
+        paramsName: str,
+        params: List,
+        parmsLables: List[str] = None,
+):
+
+    if parmsLables is None:
+        parmsLables = []
+        for param in params:
+            parmsLables.append(str(param))
+
+    testdatas = draw.testDataList()
+
+    for param, label in zip(params, parmsLables):
+        targetOut = os.path.join(pyFileDir, "target", testname,
+                                 "{}_{}".format(simCluster.name, label))
+
+        configCopy = config.copy()
+        configCopy[paramsName] = param
+        _run_single_test(simCluster, configCopy, targetOut)
+        testdatas.add_data(targetOut, "{}_{}".format(
+            simCluster.describ, label))
+
+    targetOut = os.path.join(pyFileDir, "target", testname,
+                             "{}".format(k8sCluster.name))
+    _run_single_test(k8sCluster, configCopy, targetOut)
+    testdatas.add_data(targetOut, "{}".format(k8sCluster.describ))
+
+    outfolder = os.path.join(pyFileDir, "target", "all", testname,)
+    os.system("mkdir -p {}".format(outfolder))
+    draw.all(testdatas, outfolder)
+
+
+def _run_single_test(cluster: Cluster, config: map, targetOut: str):
+
+    if os.path.isdir(targetOut):
+        print(targetOut, "already existed, so skip this test")
+        return
+
+    configOut = os.path.join(pyFileDir, "config.yaml")
+    with open(configOut, "w") as output:
+        yaml.dump(config, output)
+
+    cmd = '{} Config={} TargetFolder="{}"'.format(
+        cluster.command, "./test/config.yaml", targetOut)
+    retcode = os.system(cmd)
+    if retcode != 0:
+        print("Error when run the cmd:")
+        print(cmd)
+        sys.exit(0)
+
+    pass
+
 
 def test_compose(
-    config,
+    config: map,
     clusters: List[Cluster],
     testname: str,
     paramsName: str,
     params: List,
-    drawOnly: bool = False,
     parmsLables: List[str] = None,
 ):
     if parmsLables is None:
@@ -58,15 +117,14 @@ def test_compose(
         for param in params:
             parmsLables.append(str(param))
 
-    if not drawOnly:
-        _run_compose(config, clusters, testname,
-                     paramsName, params, parmsLables)
+    _run_compose(config, clusters, testname,
+                 paramsName, params, parmsLables)
 
     _draw_compose(clusters, testname, params, parmsLables)
 
 
 def _run_compose(
-    config,
+    config: map,
     clusters: List[Cluster],
     testname: str,
     paramsName: str,
@@ -75,29 +133,12 @@ def _run_compose(
 ):
     for param, label in zip(params, parmsLables):
         for cluster in clusters:
+
             targetOut = os.path.join(pyFileDir, "target", testname,
                                      "{}_{}".format(cluster.name, label))
-            if os.path.isdir(targetOut):
-                print(targetOut, "already existed, so skip this test")
-                continue
-
             configCopy = config.copy()
             configCopy[paramsName] = param
-            configOut = os.path.join(pyFileDir, "config.yaml")
-
-            if cluster.specialConfig is not None:
-                for k, v in cluster.specialConfig.items():
-                    configCopy[k] = v
-            with open(configOut, "w") as output:
-                yaml.dump(configCopy, output)
-
-            cmd = '{} Config={} TargetFolder="{}"'.format(
-                cluster.command, "./test/config.yaml", targetOut)
-            retcode = os.system(cmd)
-            if retcode != 0:
-                print("Error when run the cmd:")
-                print(cmd)
-                sys.exit(0)
+            _run_single_test(cluster, configCopy, targetOut)
 
 
 def _draw_compose(
--- test/test_k8s_calibre.py
+++ test/test_k8s_calibre.py
@@ -5,13 +5,13 @@ if __name__ == "__main__":
 
     config["TaskMode"] = "onePeak"
     config["NodeNum"] = 30
-
     config["NetLatency"] = "0.7"
-    config["NetLatencyVar"] = "1.22"
-    simtest.test_compose(
+
+    simtest.calibre(
         config,
-        [simtest.dcssCluster, simtest.dcssK8sCluster],
+        simtest.dcssCluster,
+        simtest.dcssK8sCluster,
         "dcssK8s_calibre",
-        "PerformanceRate",
-        [0.3],
+        "NetLatencyVar",
+        [1, 1.1, 1.2, 1.23],
     )
